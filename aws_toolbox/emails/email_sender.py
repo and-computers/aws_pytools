ead440541876bd8dfa1d0dc7f0de34804c5e8541
tosin <tosin.sonuyi@gmail.com>
2019-03-24 01:37:57 -0400

    first pass at ses boto wrapper

---
+++ aws_toolbox/emails/email_sender.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+
+import boto3
+
+
+class EmailSender():
+
+    def __init__(from_address, session=None):
+        self._from_address = from_address
+        self._client = boto3.client('ses')
+
+    def send_email(to_address, subject, message, html=False):
+
+        # type checking
+        if isinstance(to_address, str):
+            # check if commas are contained
+            if "," in to_address:
+                # if commas contained split into multiple address
+                addresses_to_pass = to_address.split(",")
+            else:
+                # if there is no comma, its one address
+                addresses_to_pass = [to_address]
+        elif isinstance(to_address, list):
+            addresses_to_pass = to_address
+
+        else:
+            raise ValueError()
+
+        self.client.send_email(
+            Source=self._from_address,
+            Destination={
+                'ToAddresses': addresses_to_pass,
+                'CcAddresses': [],
+                'BccAddresses': []
+            },
+            Message={
+                'Subject': {
+                    'Data': subject,
+                    'Charset' 'utf-8'
+                },
+                'Body': {
+                    'Text': {
+                        'Data': message,
+                        'Charset': 'utf-8'
+                    },
+                    # TODO: check if you can have both
+                    'Html': {
+                        'Data': '',
+                        'Charset': 'utf-8'
+                    }
+                }
+            }
+        )
