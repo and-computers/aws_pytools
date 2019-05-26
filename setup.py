8255259186a17f97e1e92d9490bc8a7f5de9eda3
tosin <tosin.sonuyi@gmail.com>
2019-05-25 21:30:25 -0400

    adds s3fs and boto3 dependency, but boto3 probably ignorable

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
-setup(name='aws-pytools',
-      version='0..0.1',
+setup(name='aws_toolbox',
+      version='0.0.1',
       description='A number of aws sdk wrappers',
       author='and-computers',
       author_email='jcp@mail.andcomputers.io',
