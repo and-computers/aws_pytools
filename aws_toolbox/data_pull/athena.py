3f13ad042435c6feecfa68946eac5c4d55858080
tosin <tosin.sonuyi@gmail.com>
2019-06-12 23:58:15 -0400

    skeleotn athena querying

---
+++ aws_toolbox/data_pull/athena.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import boto3
+import s3fs
+
+from typing import List
+
+
+
+class AthenaQueryEngine():
+
+    def __init__(self, database: str) -> None:
+        self._client = boto3.client('athena', region_name='us-east-1')
+        self.db_name = database
+
+    def execute_query(self, query: str) -> List:
+
+        status = None
+        while status != ATHENA_SUCCESS:
+            status = self._get_status()
+        return []
